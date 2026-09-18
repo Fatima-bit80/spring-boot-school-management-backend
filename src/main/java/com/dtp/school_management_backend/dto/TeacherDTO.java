b29593a15f9d1ee6765f4beecb591769bc7c6ac7
Fatima-bit80 <145767375+Fatima-bit80@users.noreply.github.com>
2026-09-18 05:03:58 +0300

    add TeacherDTO

---
+++ src/main/java/com/dtp/school_management_backend/dto/TeacherDTO.java
@@ -0,0 +1,54 @@
+package com.dtp.school_management_backend.dto;
+
+public class TeacherDTO {
+
+
+    private int teacherId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public TeacherDTO() {
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
