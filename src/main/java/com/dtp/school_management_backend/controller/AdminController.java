11a3a75fd8f3dcf319a58ff8497f641cc7feb9a0
Fatima-bit80 <145767375+Fatima-bit80@users.noreply.github.com>
2026-09-18 03:35:43 +0300

    add controllers

---
+++ src/main/java/com/dtp/school_management_backend/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.dtp.school_management_backend.controller;
+
+import com.dtp.school_management_backend.entity.Teacher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admins")
+public class AdminController {
+
+
+
+
+}
---
+++ src/main/java/com/dtp/school_management_backend/controller/TeacherController.java
@@ -0,0 +1,10 @@
+package com.dtp.school_management_backend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/teachers")
+public class TeacherController {
+}
