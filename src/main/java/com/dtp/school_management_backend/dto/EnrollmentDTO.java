3cdedd6ba4ea0f9a57a39cf2def7420f60d49679
Fatima-bit80 <145767375+Fatima-bit80@users.noreply.github.com>
2026-09-18 03:35:00 +0300

    fix EnrollmentDTO and StudentDTO

---
+++ src/main/java/com/dtp/school_management_backend/dto/EnrollmentDTO.java
@@ -0,0 +1,87 @@
+package com.dtp.school_management_backend.dto;
+
+import com.dtp.school_management_backend.entity.Course;
+import com.dtp.school_management_backend.entity.Student;
+import jakarta.persistence.*;
+
+public class EnrollmentDTO {
+
+    private int id;
+
+
+
+    private int grade;
+
+    private int approved;
+
+
+
+    private String courseCode;
+    private String courseName;
+    private int courseYear;
+
+    private String teacherName;
+
+
+    public EnrollmentDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getApproved() {
+        return approved;
+    }
+
+    public void setApproved(int approved) {
+        this.approved = approved;
+    }
+
+
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCourseYear() {
+        return courseYear;
+    }
+
+    public void setCourseYear(int courseYear) {
+        this.courseYear = courseYear;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+}
+
