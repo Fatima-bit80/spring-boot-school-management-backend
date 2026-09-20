5e64c39f2549b70a4c339da33eb14cbb68a0b56c
Fatima-bit80 <145767375+Fatima-bit80@users.noreply.github.com>
2026-09-20 22:47:05 +0300

    some fields are nullable

--- src/main/java/com/dtp/school_management_backend/entity/Admin.java
+++ src/main/java/com/dtp/school_management_backend/entity/Admin.java
@@ -9,20 +9,20 @@ import jakarta.persistence.*;
 public class Admin {
 
     @Id
-    @Column(name = "id", updatable = false)
+    @Column(name = "id", updatable = false,nullable = false)
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int adminId;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name",nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name",nullable = false)
     private String lastName;
 
 
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "email")
+    @JoinColumn(name = "email",nullable = false)
     private Member member;
 
     public Admin() {
